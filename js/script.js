67511017c117e60418f2aab3095c815ac70d62fc
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2023-11-08 12:50:05 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
   "use strict";
 
+    // JQ to toggle the mobile menu
+    const mobileMenuIcon = $(".mobile-menu-icon");
+    const menu = $(".navbar nav .menu");
+  
+    mobileMenuIcon.click(function () {
+      mobileMenuIcon.toggleClass("active");
+      menu.toggleClass("active");
+    });
+  
+    // Close the mobile menu when a menu item is clicked
+    const menuItems = $(".navbar nav .menu li a");
+  
+    menuItems.click(function () {
+      mobileMenuIcon.removeClass("active");
+      menu.removeClass("active");
+    });
+
   var owlExperience = $("#owl_experience");
   owlExperience.owlCarousel({
     items: 3,
